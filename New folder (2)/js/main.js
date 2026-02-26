aac43beabbf0bbf59159601f5e80f929ba5ba993
wolfsameer05-stack <wolfsameer05@gmail.com>
2026-02-26 00:49:23 -0800

    Add files via upload

---
+++ New folder (2)/js/main.js
@@ -0,0 +1,246 @@
+
+$(function () {
+	'use strict';
+	
+	var width = $(window).width();
+	var height = $(window).height();
+	$('.section.started').css({'height':height});
+	
+	/* Preloader */
+	$(window).on('load', function() {
+		$(".preloader .spinner").fadeOut(function(){
+			$('.preloader').fadeOut();
+			$('body').addClass('ready');
+		});
+	});
+
+	/* Fade animations on scroll */
+	if (width > 720) {
+		window.sr = ScrollReveal();
+		sr.reveal('.animated');
+	}
+
+	/* Typed subtitle */
+	$('.typed-title').typed({
+		stringsElement: $('.typing-title'),
+		backDelay: 5000,
+		typeSpeed: 0,
+		loop: true
+	});
+
+	/* Youtube video background */
+	var myPlayer = $("#video-bg").YTPlayer();
+
+	/* Smoothscroll */
+	if($('.section.started').length) {
+		$(window).on('scroll', function(){
+			var scrollPos = $(window).scrollTop();
+			$('.top-menu ul li a').each(function () {
+				var currLink = $(this);
+				var refElement = $(currLink.attr("href"));
+				if (refElement.offset().top <= scrollPos) {
+					$('.top-menu ul li').removeClass("active");
+					currLink.closest('li').addClass("active");
+				}
+			});
+		});
+	}
+
+	/* Top Menu */
+	if($('.section.started').length) {
+		$('.top-menu ul li a').on('click', function(){
+			var id = $(this).attr('href');
+			var h = parseFloat($(id).offset().top);
+			
+			$('body,html').animate({
+				scrollTop: h + 10
+			}, 800);
+			
+			return false;
+		});
+	}
+
+	/* Open Top Menu */
+	$('.page').on('click', '.menu-btn', function(){
+		if($('.top-menu').hasClass('active')){
+			$('.top-menu').removeClass('active');
+			$(this).removeClass('active');
+		} else {
+			$('.top-menu').addClass('active');
+			$(this).addClass('active');
+		}
+
+		return false;
+	});
+	
+	/* Hide mouse button on scroll */
+	$(window).on('scroll', function() {
+		if ($(this).scrollTop() >= height-10) {
+			$('.mouse-btn').fadeOut();
+		}
+		if ($(this).scrollTop() <= height-10) {
+			$('.mouse-btn').fadeIn();
+		}
+		if ($(this).scrollTop() <= height-10) {
+			$('.top-menu ul li').removeClass("active");
+		}
+	});
+
+	/* Pause/Play video on scroll */
+	if ($('#video-bg').length) {
+		$(window).on('scroll', function() {
+			if ($(this).scrollTop() >= height-10) {
+				$('#video-bg').YTPPause();
+			}
+			if ($(this).scrollTop() <= height-10) {
+				$('#video-bg').YTPPlay();
+			}
+		});
+	}
+	
+	/* On click mouse button, page scroll down */
+	$('.section').on('click', '.mouse-btn', function() {
+		$('body,html').animate({
+			scrollTop: height
+		}, 800);
+	});
+
+	/* Menu filled */
+	if($(window).scrollTop() > 10) {
+		$('header').addClass('filled');
+	} else {
+		$('header').removeClass('filled');
+	}
+	$(window).on('scroll', function() {
+		if($(window).scrollTop() > 10) {
+			$('header').addClass('filled');
+		} else {
+			$('header').removeClass('filled');
+		}
+	});
+
+	/* Initialize masonry items */
+	var $container = $('.box-items');
+	
+	$container.imagesLoaded(function() {
+		$container.multipleFilterMasonry({
+			itemSelector: '.box-item',
+			filtersGroupSelector: '.filters',
+			percentPosition: true,
+			gutter: 0
+		});
+	});
+	
+
+	/* 12. Initialize masonry filter */
+	$('.filters label').on('change', 'input[type="radio"]', function() {
+		if ($(this).is(':checked')) {
+			$('.f_btn').removeClass('active');
+			$(this).closest('.f_btn').addClass('active');
+		}
+		/* Refresh Portfolio magnific popup */
+		$('.has-popup').magnificPopup({
+			type: 'inline',
+			overflowY: 'auto',
+			closeBtnInside: true,
+			mainClass: 'mfp-fade'
+		});
+	});
+
+	/* Portfolio magnific popup */
+	$('.has-popup').magnificPopup({
+		type: 'inline',
+		overflowY: 'auto',
+		closeBtnInside: true,
+		mainClass: 'mfp-fade'
+	});
+	
+	/* gallery */
+	$('.post-lightbox').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+		}
+	});
+	
+	/* Validate contact form */
+	$("#cform").validate({
+		rules: {
+			name: {
+				required: true
+			},
+			tel: {
+				required: true
+			},
+			message: {
+				required: true
+			},
+			subject: {
+				required: true
+			},
+			email: {
+				required: true,
+				email: true
+			}
+		},
+		success: "valid",
+		submitHandler: function() {
+			$.ajax({
+				url: 'mailer/feedback.php',
+				type: 'post',
+				dataType: 'json',
+				data: 'name='+ $("#cform").find('input[name="name"]').val() + '&tel='+ $("#cform").find('input[name="tel"]').val() + '&email='+ $("#cform").find('input[name="email"]').val() + '&subject='+ $("#cform").find('input[name="subject"]').val() + '&message=' + $("#cform").find('textarea[name="message"]').val(),
+				beforeSend: function() {
+				
+				},
+				complete: function() {
+				
+				},
+				success: function(data) {
+					$('#cform').fadeOut();
+					$('.alert-success').delay(1000).fadeIn();
+				}
+			});
+		}
+	});
+	
+	/* Validate contact form */
+	$("#blog-form").validate({
+		rules: {
+			name: {
+				required: true
+			},
+			message: {
+				required: true
+			},
+			email: {
+				required: true,
+				email: true
+			}
+		},
+		success: "valid",
+		submitHandler: function() {
+			$('#blog-form').fadeOut();
+			$('.alert-success').delay(1000).fadeIn();
+		}
+	});
+
+});
+
+$(function() {
+$('body').append('<div class="fix-bts" style=" position: fixed; top: 50vh; right: -74px; z-index: 1111; transition: all 0.2s ease 0s;"> <a target="blank" title="Check our portfolio on Themeforest" href="https://1.envato.market/JERja" class="fix-btn brand" style=" position: relative; margin-bottom: 5px; padding: 0 10px; display: block; height: 38px; line-height: 38px; background: #fff; text-transform: uppercase; font-size: 11px; box-shadow: 0 5px 30px rgba(0, 0, 0, 0.1); font-weight: 700; text-decoration: none; color: #000; "><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="415.441px" height="415.441px" viewBox="0 0 415.441 415.441" xml:space="preserve" style=" width: 18px; height: 38px; display: inline-block; vertical-align: middle; margin-right: 10px; "> <g> <g> <path fill="#7cb342" d="M324.63,22.533C135.173,226.428,80.309,371.638,80.309,371.638c41.149,47.743,111.28,43.72,111.28,43.72 c73.921,2.31,119.192-43.522,119.192-43.522c91.861-92.516,80.549-355.302,80.549-355.302 C372.769-23.891,324.63,22.533,324.63,22.533z"></path> <path fill="#7cb342" d="M32.369,181.983c0,0-28.983,57.964,18.859,155.495L178.367,58.01C176.916,58.538,63.691,98.037,32.369,181.983z"></path> </g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </svg>Beshleyua</a> <a target="blank" title="Buy HTML template on Themeforest" href="https://1.envato.market/KmWav" class="fix-btn bh" style=" position: relative; margin-bottom: 5px; padding: 0 10px; display: none; height: 38px; line-height: 38px; background: #fff; text-transform: uppercase; font-size: 11px; box-shadow: 0 5px 30px rgb(0 0 0 / 10%); font-weight: 700; text-decoration: none; color: #000; "><svg id="Capa_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg" style=" width: 18px; height: 38px; display: inline-block; vertical-align: middle; margin-right: 10px; "><path d="m504.399 185.065c-6.761-8.482-16.904-13.348-27.83-13.348h-98.604l-53.469-122.433c-3.315-7.591-12.157-11.06-19.749-7.743-7.592 3.315-11.059 12.158-7.743 19.75l48.225 110.427h-178.458l48.225-110.427c3.315-7.592-.151-16.434-7.743-19.75-7.591-3.317-16.434.15-19.749 7.743l-53.469 122.434h-98.604c-10.926 0-21.069 4.865-27.83 13.348-6.637 8.328-9.086 19.034-6.719 29.376l52.657 230c3.677 16.06 17.884 27.276 34.549 27.276h335.824c16.665 0 30.872-11.216 34.549-27.276l52.657-230.001c2.367-10.342-.082-21.048-6.719-29.376zm-80.487 256.652h-335.824c-2.547 0-4.778-1.67-5.305-3.972l-52.657-229.998c-.413-1.805.28-3.163.936-3.984.608-.764 1.985-2.045 4.369-2.045h85.503l-3.929 8.997c-3.315 7.592.151 16.434 7.743 19.75 1.954.854 3.99 1.258 5.995 1.258 5.782 0 11.292-3.363 13.754-9l9.173-21.003h204.662l9.173 21.003c2.462 5.638 7.972 9 13.754 9 2.004 0 4.041-.404 5.995-1.258 7.592-3.315 11.059-12.158 7.743-19.75l-3.929-8.997h85.503c2.384 0 3.761 1.281 4.369 2.045.655.822 1.349 2.18.936 3.983l-52.657 230c-.528 2.301-2.76 3.971-5.307 3.971z"></path><path d="m166 266.717c-8.284 0-15 6.716-15 15v110c0 8.284 6.716 15 15 15s15-6.716 15-15v-110c0-8.284-6.715-15-15-15z"></path><path d="m256 266.717c-8.284 0-15 6.716-15 15v110c0 8.284 6.716 15 15 15s15-6.716 15-15v-110c0-8.284-6.716-15-15-15z"></path><path d="m346 266.717c-8.284 0-15 6.716-15 15v110c0 8.284 6.716 15 15 15s15-6.716 15-15v-110c-.001-8.284-6.716-15-15-15z"></path></svg>Buy HTML</a></div>');
+$('.fix-bts').on('mouseenter', function() {
+$('.fix-bts').css({'right':'0'});
+});
+$('.fix-bts').on('mouseleave', function() {
+$('.fix-bts').css({'right':'-74px'});
+});
+});
